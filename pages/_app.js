a187129306ed74903d6e8ac80555477d58c45294
Gregory Bluvshteyn <greg.bluvshteyn@gmail.com>
2022-02-15 15:01:14 -0500

    css example

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// This default export is required in a new `pages/_app.js` file.
+import '../styles.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ pages/styled/page2/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './RedSquare.module.css';
+
+
+export default () => {
+  return <div className={styles.redSquare}>
+    This is the Red Square
+  </div>
+}
